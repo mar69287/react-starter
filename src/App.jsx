830cdece01b9b64316bcc8c478a63a9ffd725f15
mar69287 <marcoa.ruiz97@gmail.com>
2024-11-27 12:12:34 -0800

    import pages using index

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,22 @@
-import { Routes, Route } from 'react-router-dom'
-import './App.css'
-import Home from './pages/Home'
-import { AuthProvider } from './context/AuthContext'
-import PrivateRoutes from './utilities/PrivateRoutes'
-import Dashboard from './pages/Dashboard'
-import Navigation from './components/Navigation'
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import { AuthProvider } from "./context/AuthContext";
+import PrivateRoutes from "./utilities/PrivateRoutes";
+import Navigation from "./components/Navigation";
+import { Dashboard, Home } from "./pages/";
 
 function App() {
-
   return (
     <AuthProvider>
       <Navigation />
-      <main
-        className='main'
-      >
-        <Routes>
-          <Route element={<PrivateRoutes />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Route>
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </main>
+      <Routes>
+        <Route element={<PrivateRoutes />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
